918a6a4fd47b50277604a6cd973e7c386ef2cb3c
TrevorJA <tja73@cornell.edu>
2025-10-29 02:14:15 -0400

    uphop

--- S9_run_figure_generation.sh
+++ S9_run_figure_generation.sh
@@ -16,18 +16,25 @@ np=$(($SLURM_NTASKS_PER_NODE * $SLURM_NNODES))
 # make directories
 mkdir -p logs figures
 
+
+# Ensemble diagnotic plot
+# python3 10_plot_streamflow_scenario_comparison.py delMontague
+
+
 # SSI copula diagnostic figures (for Supplementary Information)
-python3 07_compare_copula_parameters.py
+# python3 07_compare_copula_parameters.py
 
 
 # SSI copula diagnostic figures (for Supplementary Information)
 # python3 09_plot_ssi_copula_diagnostics.py
 
+# # 4-panel drought return period comparison
+python3 09_plot_drought_frequency.py comparison
+
 # Ensemble diagnostic plots for delMontague
 # python3 10_plot_streamflow_scenario_comparison.py delMontague
 
-# # 4-panel drought return period comparison
-# python3 09_plot_drought_frequency.py comparison
+
 
 # # 4-panel storage zone probability comparison
 # python3 09_plot_reservoir_storage_zone_probabilities.py comparison
