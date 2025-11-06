1d56997abd3cb6241487336a6b5846367aefadf1
Trevor Amestoy <89143989+TrevorJA@users.noreply.github.com>
2025-11-06 14:55:21 -0500

    up

--- S9_run_SI_scripts.sh
+++ S9_run_SI_scripts.sh
@@ -19,4 +19,6 @@ mkdir -p logs figures
 
 python3 SI1_plot_shortage_occurrence_by_day.py stationary_ensemble
 
+python3 SI2_plot_satisficing_by_event.py stationary_ensemble 3
 python3 SI2_plot_satisficing_by_event.py stationary_ensemble 6
+python3 SI2_plot_satisficing_by_event.py stationary_ensemble 12
